976107f33abbaacbfa15940647cd3310979b3d36
Muhil <muhil.vampire.2@gmail.com>
2025-08-06 21:41:20 +0000

    the my services star will be same color as "Professional Assignment Writ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -368,13 +368,12 @@ export default function Home() {
         <section id="services" className="py-16 md:py-24 bg-black">
           <div className="container mx-auto px-4">
             <div className="flex justify-center mb-4">
-              <Star className="w-12 h-12 text-yellow-500" style={{ fill: 'url(#fire-gradient)' }} />
+              <Star className="w-12 h-12 text-blue-500" style={{ fill: 'url(#navy-blue-gradient)' }} />
               <svg width="0" height="0" style={{ position: 'absolute' }}>
                 <defs>
-                  <linearGradient id="fire-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-                    <stop offset="0%" style={{ stopColor: '#ff4500', stopOpacity: 1 }} />
-                    <stop offset="50%" style={{ stopColor: '#ff8c00', stopOpacity: 1 }} />
-                    <stop offset="100%" style={{ stopColor: '#ffd700', stopOpacity: 1 }} />
+                  <linearGradient id="navy-blue-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                    <stop offset="0%" style={{ stopColor: 'hsl(210, 100%, 25%)', stopOpacity: 1 }} />
+                    <stop offset="100%" style={{ stopColor: 'hsl(210, 100%, 50%)', stopOpacity: 1 }} />
                   </linearGradient>
                 </defs>
               </svg>
@@ -611,3 +610,5 @@ export default function Home() {
     </div>
   );
 }
+
+    
